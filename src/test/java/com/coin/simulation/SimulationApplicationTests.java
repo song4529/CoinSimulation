ce6c44fc7c4228ff5f3318c87cf31f8af910a5b2
song4529 <gusdn4529@gmail.com>
2023-06-23 21:49:00 +0900

    first commit

---
+++ src/test/java/com/coin/simulation/SimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin.simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
